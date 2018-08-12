de1e8478dda589798d1ab16c42a0fa13135c4e56
navib <ivan.baena.dev@gmail.com>
2018-08-12 12:17:46 -0400

    user save to db oauth

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ ivan: 'baena' });
-});
+require('./routes/authRoutes')(app);
 
-//PRODUCTION/LOCAL
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
